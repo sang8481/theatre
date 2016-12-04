92eb158b21025877d4abfc8112ccd919c07291b4
Lee SangsSu <sang8481@naver.com>
2016-12-04 21:29:23 +0900

    Add DB table creators

--- src/DBInitializer.java
+++ src/DBInitializer.java
@@ -8,7 +8,85 @@ public class DBInitializer{
 
 	}
 	public void allTableInitialize(){
+		String[] querys = new String[]{
+			"create table customer(
+				id char(20) not null,
+				password char(20) not null,
+				name char(20) not null,
+				phone char(20) not null,
+				email char(40) not null,
+				address char(50) not null,
+				birth char(10) not null,
+				vip char(3) default 'no',
+				point number,
+				primary key(id)
+			)",
+			"create table theater(
+				id char(20) not null,
+				name char(20) not null,
+				address char(50) not null,
+				phone char(20) not null,
+				primary key(id)
 
+			)",
+			"create table auditorium(
+				id char(20) not null,
+				theater_id char(20) not null,
+				primary key(id),
+				foreign key(theater_id) references theater(id)
+			)",
+			"create table seat(
+				id char(4) not null,
+				theater_id char(20) not null,
+				auditorium_id char(20) not null,
+				primary key(id),
+				foreign key(theater_id) references theater(id),
+				foreign key(auditorium_id) references auditorium(id)
+			)",
+			"create table movie(
+				id char(20) not null,
+				name char(40) not null,
+				director char(40) not null,
+				grade char(20) not null,
+				description char(100) not null,
+				primary key(id),
+			)",
+			"create table schedule(
+				time char(10) not null,
+				auditorium_id char(20) not null,
+				movie_id char(20) not null,
+				theater_id char(20) not null,
+				primary key(time),
+				foreign key(auditorium_id) references auditorium(id),
+				foreign key(movie_id) references movid(id),
+				foreign key(theater_id) references theater(id)
+			)",
+		   "create table ticket(
+				id char(20) not null,
+				customer_id char(20) not null,
+				theater_id char(20) not null,
+				auditorium_id char(20) not null,
+				schedule_time char(10) not null,
+				seat_id char(20) not null,
+				movie_id char(20) not null,
+				pay_type char(10) not null,
+				pay_method char(10) not null,
+				pay_state char(10) not null,
+				price number,
+				primary key(id),
+				foreign key(customer_id) references customer(id),
+				foreign key(theater_id) references theater(id),
+				foreign key(auditorium_id) references auditorium(id),
+				foreign key(schedule_time) references schedule(time),
+				foreign key(seat_id) references seat(id),
+				foreign key(movie_id) references movie(id),
+				
+			)"
+		};
+		for(String query : querys){
+			this.queryConnector.executeWith(query);
+		}
+		System.out.println("table initialze completed.");
 	}
 
 	public void dropAllTables(){
